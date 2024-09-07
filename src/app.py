16ecba728d845890b09b88eb9b9892a5d6ca34db
Nestorfron <nestorfrones07@hotmail.com>
2024-09-07 18:16:01 -0300

    models and endpoints

--- src/app.py
+++ src/app.py
@@ -10,6 +10,8 @@ from api.models import db
 from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
+from flask_jwt_extended import JWTManager
+
 
 # from models import Person
 
@@ -28,11 +30,16 @@ else:
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = os.environ.get("FLASK_APP_KEY")
+
+
 MIGRATE = Migrate(app, db, compare_type=True)
 db.init_app(app)
 
 # add the admin
 setup_admin(app)
+
+JWTManager(app)
 
 # add the admin
 setup_commands(app)
